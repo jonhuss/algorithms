2fdbb0e1e7b790d3a580af66784cf1992f022cda
Jon Huss <jon.huss.827@gmail.com>
2018-05-12 15:12:57 -0500

    Initial commit

---
+++ binary_search.py
@@ -0,0 +1,16 @@
+def binary_search_func(list, item):
+	low = 0
+	high = len(list) - 1
+
+	while low <= high:
+		mid = (low + high) // 2
+		guess = list[mid]
+
+		if guess == item:
+			return mid
+		elif guess > item:
+			high = mid - 1
+		else:
+			low = mid + 1
+
+	return None
